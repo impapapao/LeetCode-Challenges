6fd4c8d29c0c8f4c163b4d99afca370bb8eafd55
impaolo15 <john.paolo1724@gmail.com>
2023-03-22 08:29:58 +0800

    Array

---
+++ 3Sum/ThreeSum.cs
@@ -0,0 +1,55 @@
+public class Solution {
+    public IList<IList<int>> ThreeSum(int[] nums) {
+        IList<IList<int>> result = new List<IList<int>>();
+
+        if(nums.Length < 3)
+        {
+            return result;
+        }
+
+        Array.Sort(nums);
+
+        for (int i = 0; i < nums.Length - 2; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int j = i + 1;
+            int k = nums.Length - 1;
+
+            while (j < k)
+            {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0)
+                {
+                    result.Add(new List<int>() { nums[i], nums[j], nums[k] });
+                    while (j < k && nums[j] == nums[j + 1]) j++;
+                    while (j < k && nums[k] == nums[k - 1]) k--;
+                    j++;
+                    k--;
+                }
+                else if (sum < 0)
+                {
+                    j++;
+                }
+                else
+                {
+                    k--;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public int[] TwoSum(int[] nums, int target) {
+        var map = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++) {
+            int complement = target - nums[i];
+            if (map.ContainsKey(complement)) {
+                return new int[] { map[complement], i };
+            }
+            map[nums[i]] = i;
+        }
+        throw new ArgumentException("No two sum solution");
+    }
+}
